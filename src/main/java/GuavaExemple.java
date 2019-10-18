9e8c8427d308880e36f86feb9288709caf9ea772
Cedric Caballol <cedric.caballol@hotmail.fr>
2019-10-18 12:57:49 +0200

    fix merge

--- src/main/java/GuavaExemple.java
+++ src/main/java/GuavaExemple.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class GuavaExemple{
 	 
@@ -6,19 +7,40 @@ public class GuavaExemple{
 		 bus = EventBusService.getInstance();
 		 String[] ring = {"P1", "P2"};
 
-	     Process p1 = new Process(ring[0], ring);
-	     Process p2 = new Process(ring[1], ring);
+		 Scanner sc = new Scanner(System.in);
+		 int answer = -1;
+
 	     // Lancement du Token une fois tous les processus créés
 	     Token t = new Token(ring[0]);
 	     bus.postEvent(t);
 
-	     try{
-		 	Thread.sleep(3000);
-	     }catch(Exception e){
-		 	e.printStackTrace();
-	     }
+	     while (true) {
+			 System.out.println("run tests for lamport messages : 0");
+			 System.out.println("run tests for broadcast messages : 1");
+			 System.out.println("run tests for dedicated messages : 2");
+			 System.out.println("exit program : anything else than precedent commands");
+			 try {
+				 answer = sc.nextInt();
+			 } catch (Exception e) {
+				 break;
+			 }
 
-	     p1.stop();
-	     p2.stop();
+			 if (answer < 0 || answer > 2) {
+				 break;
+			 }
+
+			 RunTests answer_enum = RunTests.fromValue(answer);
+			 Process p1 = new Process(ring[0], ring, answer_enum);
+			 Process p2 = new Process(ring[0], ring, answer_enum);
+
+			 try {
+				 Thread.sleep(2000);
+			 } catch (Exception e) {
+				 e.printStackTrace();
+			 }
+
+			 p1.stop();
+			 p2.stop();
+		 }
 	 }
 }
--- src/main/java/Process.java
+++ src/main/java/Process.java
@@ -12,8 +12,9 @@ public class Process implements Runnable {
     private String[] ring;
     private boolean isInSectionCritique;
     private boolean attendJeton;
+    private RunTests runTests;
 
-    public Process(String name, String[] ring) {
+    public Process(String name, String[] ring, RunTests runTests) {
 
         this.bus = EventBusService.getInstance();
         this.bus.registerSubscriber(this); // Auto enregistrement sur le bus afin que les methodes "@Subscribe" soient invoquees automatiquement.
@@ -27,6 +28,7 @@ public class Process implements Runnable {
         this.attendJeton = false;
         this.alive = true;
         this.dead = false;
+        this.runTests = runTests;
         this.thread.start();
     }
 
@@ -107,6 +109,41 @@ public class Process implements Runnable {
         this.isInSectionCritique = false;
     }
 
+    private void display_test(LamportMessage b1, LamportMessage b2) {
+        System.out.println(Thread.currentThread().getName() + " send : " + b1.getMachin() +
+                " with lamport clock : " + b1.getEstampille());
+        System.out.println(Thread.currentThread().getName() + " send : " + b2.getMachin() +
+                " with lamport clock : " + b2.getEstampille());
+    }
+
+    private void postEvents(LamportMessage b1, LamportMessage b2) {
+        bus.postEvent(b1);
+        bus.postEvent(b2);
+    }
+
+    private void runLamportTests() {
+        System.out.println("Running lamport tests");
+        LamportMessage b1 = new LamportMessage("ga", ++this.clock);
+        LamportMessage b2 = new LamportMessage("bu", ++this.clock);
+        display_test(b1,b2);
+        postEvents(b1,b2);
+    }
+
+    private void runBroadcastTests() {
+        System.out.println("Running broadcast tests");
+        BroadcastMessage b1 = new BroadcastMessage("ga", ++this.clock, Thread.currentThread().getName());
+        BroadcastMessage b2 = new BroadcastMessage("bu", ++this.clock, Thread.currentThread().getName());
+        display_test(b1,b2);
+        postEvents(b1,b2);
+    }
+
+    private void runDedicatedTests () {
+        System.out.println("Running dedicated tests");
+        DedicatedMessage b1 = new DedicatedMessage("ga", ++this.clock, "P1");
+        DedicatedMessage b2 = new DedicatedMessage("bu", ++this.clock, "P2");
+        display_test(b1,b2);
+        postEvents(b1,b2);
+    }
 
     public void run() {
         int loop = 0;
@@ -119,6 +156,23 @@ public class Process implements Runnable {
                 this.request();  // bloquant jusqu'à l'obtention du token
                 // Do some stuff
                 Thread.sleep(500);
+                if (Thread.currentThread().getName().equals("P1")) {
+                    switch (this.runTests) {
+                        case LAMPORT :
+                            runLamportTests();
+                            break;
+                        case BROADCAST :
+                            runBroadcastTests();
+                            break;
+                        case DEDICATED :
+                            runDedicatedTests();
+                            break;
+                        default :
+                            System.out.println("Exiting program...");
+                            break;
+                    }
+                }
+
                 System.out.println(this.thread.getName() + " est en section critique");
                 this.release(); // revoie le token au prochain process sur l'anneau
             } catch (Exception e) {
